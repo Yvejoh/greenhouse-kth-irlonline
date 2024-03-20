61ebb56f65bfae17918ec325191d77e2a331e4de
Johann Yvetot <johann.yvetot@knowit.se>
2024-03-20 14:12:32 +0100

    Updated user creation with repository

--- create_user.php
+++ create_user.php
@@ -1,28 +1,23 @@
-<!-- connect to database -->
 <?php
-require_once ("dbconnect_user.php");
-?>
 
-<!-- save user input in variables -->
-<?php
+require_once("users/repository.php");
+
+$repo = UserRepository::get();
 $username = $_POST['username'];
-$password = $_POST['password'];
-$password = password_hash($password, PASSWORD_BCRYPT);
 
-// insert user input into database as new user
-$sql = "INSERT INTO IRLusers (username, psswrd)
-VALUES ('$username', '$password')";
-
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-    header("location: login.php");
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if ($repo->findByUsername($username)) {
+    echo "Already exists!";
+    return;
 }
 
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-//$hash = password_hash($password, PASSWORD_DEFAULT);
+if (!$repo->create($username, $password)) {
+    echo "Failed to create user!";
+    return;
+}
+
+header("location: login.php")
 
 ?>
 
